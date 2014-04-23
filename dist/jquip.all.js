cf234a0fa151d7fa9f1a8bdcdd46ad49a722c441
root <root@boris-VirtualBox.(none)>
2014-04-23 18:37:54 +0300

    Rebuilding library after changes with events triggering

--- dist/jquip.all.js
+++ dist/jquip.all.js
@@ -1607,7 +1607,7 @@
     };
       p['trigger'] = function (evt) {
           return this['each'](function () {
-              if ((evt == "click" || evt == "blur" || evt == "focus") && this[evt])
+              if ((evt == "click") && this[evt])
                   return this[evt]();
               if (doc.createEvent) {
                   var e = doc.createEvent('Events');
